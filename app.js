67315f6a68ba225be7371b3c2fa842b6ee74fd94
EpochAndFriends <maximilianobrocato@gmail.com>
2023-07-21 16:44:16 -0300

    Trabajando con formularios: Agregar páginas de registro y login

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ const port = 3030;
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) =>
+app.get("/home", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+
+// Ruta para la página de registro de usuario
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+
+// Ruta para la página de inicio de sesión
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
 );
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
+
+app.post("/home", (req, res) => 
+res.sendFile(path.join(__dirname, "views", "home.html"))
+);
